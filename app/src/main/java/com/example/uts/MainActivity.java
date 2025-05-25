4c98b2b0503e9de4ffbcb901deae9cdea52dcdf1
Hhanzz <151647894+hhanzz08@users.noreply.github.com>
2025-05-25 20:17:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.uts;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+        tvWelcome = findViewById(R.id.tvWelcome);
+
+        String name = getIntent().getStringExtra("user_name");
+        if (name != null && !name.isEmpty()) {
+            tvWelcome.setText("Selamat Datang, " + name);
+        } else {
+            tvWelcome.setText("Selamat Datang");
+        }
+    }
+}
